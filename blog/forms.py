ef148303d2f505f92c562c9c829735c8180d9bd0
T-bear <torbjorn.holgersson87@gmail.com>
2017-04-28 16:14:08 +0200

    fixing gitignore

--- blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-from django import forms
-
-from django import forms
-
-class PostsAdd(forms.Form):
-    titel = forms.CharField(label='titel', max_length=100)
-    blog_post = forms.CharField(label='blog_post', max_length=100)
